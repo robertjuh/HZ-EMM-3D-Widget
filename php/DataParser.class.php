bc38aff85b9be3954ff0f7cc6fa8789eec488244
unknown <walh0028@HZ6270.myhz.nl>
2015-11-10 11:31:53 +0100

    testcommit voor datastructureBranch

--- php/DataParser.class.php
+++ php/DataParser.class.php
@@ -11,7 +11,7 @@ class DataParser {
 	function parseDataRDF1() {
 		
 				
-					file_put_contents('php://stderr', print_r('---json decode resultaat:', TRUE));
+					file_put_contents('php://stderr', print_r('---json decode resultaatje:', TRUE));
 					file_put_contents('php://stderr', print_r($this, TRUE));
 
 					
