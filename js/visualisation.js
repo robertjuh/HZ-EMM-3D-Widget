780385b56f056dc8cdd8a665f1dcc3c9c98c156f
Anton <anton.bil@hz.nl>
2015-12-18 17:54:55 +0100

    containerdiv position absolute

--- js/visualisation.js
+++ js/visualisation.js
@@ -915,7 +915,7 @@ function checkGeometryTypeAndSlice(intersects, urlname){
       css: "position:relative;height:"+$("#"+containerDivDescription).height()+ "px;width: "+$("#"+containerDivDescription).width()+ "px;display:inline-block;"*/
       }).prependTo('#'+BODYCONTENTDIV);
       $("#"+containerDivDescription).appendTo('#'+EMMCONTAINERDIV);
-      $("#"+containerDivDescription).attr("css","position:absolute;");
+      $("#"+containerDivDescription).css("position","absolute");
       $("#"+sliderDiv).appendTo('#'+EMMCONTAINERDIV);
       //TODO see why vertical-align: top; has no effect here on #sliderDiv (in css it works although!)
       $("#"+EMMCONTAINERDIV).css("position","relative").css("height",""+$("#"+containerDivDescription).height()+ "px")
