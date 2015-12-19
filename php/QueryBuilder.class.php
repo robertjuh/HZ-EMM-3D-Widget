3abfc35ec00a38db02af12127fb7dfae8bb20b00
Anton <anton.bil@hz.nl>
2015-12-19 16:41:10 +0100

    added try-catch if node exists

--- php/QueryBuilder.class.php
+++ php/QueryBuilder.class.php
@@ -16,7 +16,7 @@ class QueryBuilder {
 			  ?s ?p ?o
 			  FILTER(EXISTS { ?s a uri:Categorie-3ASKOS_Concept }  || EXISTS{ ?s a  uri:Categorie-3ASocKrt_Concept} )
 			}
-			limit 3000
+			limit 4000
 		';
 
 	//parameters passed by calling javascript
