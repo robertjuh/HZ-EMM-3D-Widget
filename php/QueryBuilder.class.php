84108370063f55a13f4022e6ea20661f8634f5c9
Anton <anton.bil@hz.nl>
2015-12-19 16:23:13 +0100

    added timit 150 to rdf-query

--- php/QueryBuilder.class.php
+++ php/QueryBuilder.class.php
@@ -16,7 +16,7 @@ class QueryBuilder {
 			  ?s ?p ?o
 			  FILTER(EXISTS { ?s a uri:Categorie-3ASKOS_Concept }  || EXISTS{ ?s a  uri:Categorie-3ASocKrt_Concept} )
 			}
-			limit 150
+			limit 500
 		';
 
 	//parameters passed by calling javascript
